e0b5665556861d5b789d966a2af9298beabdb120
AceDJH <460745177@qq.com>
2019-11-04 15:43:23 +0800

    照相与相册功能 照相功能未做适配，拍照后可能出现闪退现象

---
+++ app/src/main/java/com/example/face/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.face;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private ImageView ivPicture;
+    private Uri imageUri;
+    private Button btnTakePhoto;
+    private Button btnFromAlbum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTakePhoto = findViewById(R.id.btn_photo);
+        btnFromAlbum = findViewById(R.id.btn_choose_from_album);
+        ivPicture = findViewById(R.id.iv_picture);
+        btnTakePhoto.setOnClickListener(this);
+        btnFromAlbum.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_photo:
+                File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+//                    与manifest中的配置相关
+                    imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.face.fileprovider", outputImage);
+                } else {
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+                break;
+            case R.id.btn_choose_from_album:
+                select();
+            default:
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        ivPicture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            case CHOOSE_PHOTO:
+                handleSelect(data);
+            default:
+                break;
+        }
+    }
+
+    public void select() {
+        String[] permissions=new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        //进行sdcard的读写请求
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,permissions, CHOOSE_PHOTO);
+        }
+        else{
+            openGallery(); //打开相册，进行选择
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CHOOSE_PHOTO:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    openGallery();
+                }
+                else{
+                    Toast.makeText(this,"读相册的操作被拒绝",Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    //打开相册,进行照片的选择
+    private void openGallery(){
+        Intent intent=new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    //选择后照片的读取工作
+    private void handleSelect(Intent intent){
+        Cursor cursor=null;
+        Uri uri=intent.getData();
+        //如果直接是从"相册"中选择，则Uri的形式是"content://xxxx"的形式
+        if("content".equalsIgnoreCase(uri.getScheme())){
+            cursor= getContentResolver().query(uri,null,null,null,null);
+            if(cursor.moveToFirst()){
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                String path=cursor.getString(columnIndex);
+                Bitmap bitmap= BitmapFactory.decodeFile(path);
+                ivPicture.setImageBitmap(bitmap);
+            }
+        }
+        else{
+            Log.i("other","其它数据类型.....");
+        }
+        cursor.close();
+    }
+}
+
